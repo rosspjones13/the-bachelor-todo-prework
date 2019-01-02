f9fe4921a6df218304df9e1647ce9ee3eb47271e
Ross Jones <rossmosis.jones@gmail.com>
2019-01-02 01:30:48 -0500

    created methods to return bachelor details

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split.first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, details|
+    details.each do |contestant, info|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter = 0
+
+  data.each do |season, details|
+    details.each do |contestant, info|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, details|
+    details.each do |contestant, info|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  average_age = 0
+  count_contestant = 0
+
+  data[season].each do |contestant|
+    average_age += contestant["age"].to_f
+    count_contestant += 1
+  end
+  (average_age / count_contestant).round
 end
